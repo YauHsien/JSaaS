13f2674480a66b3054888d7de4e6b6614485a995
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2019-04-04 18:00:44 +0800

    add a .net core solution

---
+++ JobSuffler_as_a_Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JobSuffler_as_a_Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
